82f75d05343c6be9b60f76cf03851b00dc928b45
Boy Aditya <boy.adityarr@gmail.com>
2025-05-10 16:26:00 +0700

    style(button): Add custom shadcn variant and size

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+// import Button from "./components/Button.jsx";
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh gap-1">
+      <Button size="custom">Custom Default</Button>
+      <Button size="custom" variant="lite">Custom Lite</Button>
+    </div>
   );
 }
 
